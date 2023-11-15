3e32b7c79c44d5cae767575b5200c60f7102c0a0
menmozhii <125111018+menmozhii@users.noreply.github.com>
2023-11-15 12:22:43 +0530

    Add files via upload

---
+++ scrpit_dom.js
@@ -0,0 +1,56 @@
+
+var div=document.createElement("div");
+div.setAttribute("class","hell");
+div.style.textAlign="center";
+div.style.marginTop="200px";
+div.style.color="Green";
+document.body.append(div);
+
+
+var  hell = document.querySelector(".hell");
+setTimeout(function(){
+    hell.innerHTML="10";
+    setTimeout(function(){
+hell.innerHTML="9";
+setTimeout(function(){
+    hell.innerHTML="8";
+    setTimeout(function(){
+        hell.innerHTML="7";
+        setTimeout(function(){
+            hell.innerHTML="6";
+
+    setTimeout(function(){
+        hell.innerHTML="5";
+
+    
+    setTimeout(function(){
+        hell.innerHTML="4";
+        setTimeout(function(){
+            hell.innerHTML="3";
+            
+            setTimeout(function(){
+                hell.innerHTML="2";
+                setTimeout(function(){
+                    hell.innerHTML="1";
+
+                    setTimeout(function(){
+            hell.innerHTML="HAPPY INDEPENDENCE DAY!!!";
+             
+        },2000)
+    },1000)
+
+
+        },1000)
+
+        },1000)
+
+    },1000)
+        },1000)
+
+    },1000)
+
+},1000)
+    },1000)
+
+},1000)
+},1000)
